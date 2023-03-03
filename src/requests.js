80397296a4ce4512bd1325aa3140dd89533f07c9
YemiHa <yemi.haile07@gmail.com>
2023-03-03 18:19:49 +0300

    Netflix project finished

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "2c167c91acf57137022ae031b4232103";
+
+const requests = {
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
+// https://api.themoviedb.org/3//discover/movie?api_key=c988b88cef092958b9789a08094d1471&with_genres=99
